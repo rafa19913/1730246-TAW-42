f4a9dcec6ea19fe4669304d2c328de0a9e1a3dcd
rafa19913 <45272701+rafa19913@users.noreply.github.com>
2020-05-22 09:16:26 -0500

    bd actualizado

--- Ejercicio MVC poo/bd/script.sql
+++ Ejercicio MVC poo/bd/script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2020 a las 15:56:14
+-- Tiempo de generación: 22-05-2020 a las 16:15:38
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.30
 
@@ -24,34 +24,64 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `universidad_carrera`
+-- Estructura de tabla para la tabla `estudiante`
 --
 
-CREATE TABLE `universidad_carrera` (
-  `id` int(11) NOT NULL,
-  `id_universidad` int(4) NOT NULL,
-  `id_carrera` int(4) NOT NULL
+CREATE TABLE `estudiante` (
+  `id` int(5) NOT NULL,
+  `cedula` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `promedio` float NOT NULL,
+  `edad` int(5) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_carrera` int(4) NOT NULL,
+  `id_universidad` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `estudiante`
+--
+
+INSERT INTO `estudiante` (`id`, `cedula`, `nombre`, `apellidos`, `promedio`, `edad`, `fecha`, `id_carrera`, `id_universidad`) VALUES
+(7, 'asdfaskdfnljkasdfasdf', 'José Rafael', 'Perez Rodríguez', 85, 20, '2020-05-22', 1, 1),
+(8, 'ghdfgjdfjfgdjfdg', 'Paola', 'Torres Gudiño', 90, 21, '2020-05-22', 3, 2),
+(9, 'hgjdfjdfgjfgjdfg', 'Karina', 'Gomez Miranda', 90, 20, '2020-05-22', 3, 2),
+(10, 'hjkjhlgjklgh', 'Juan', 'Pérez', 90, 25, '2020-05-22', 2, 1),
+(11, 'shfgshsdfghs', 'Carlos Gamaliel', 'Rodríguez Medina', 100, 25, '2020-05-22', 1, 1);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `universidad_carrera`
+-- Indices de la tabla `estudiante`
 --
-ALTER TABLE `universidad_carrera`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_universidad` (`id_universidad`),
+  ADD KEY `id_carrera` (`id_carrera`) USING BTREE;
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `universidad_carrera`
+-- AUTO_INCREMENT de la tabla `estudiante`
 --
-ALTER TABLE `universidad_carrera`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `estudiante`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_carrera`) REFERENCES `carrera` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `estudiante_ibfk_2` FOREIGN KEY (`id_universidad`) REFERENCES `universidad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
